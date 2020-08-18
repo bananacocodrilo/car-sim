5ac7e44567118eb591e394c7d9c555003cb48880
bananacocodrilo <antonio.mkp@gmail.com>
2020-08-18 08:43:19 +0200

    🏷️ Export new classes

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,3 +1,9 @@
 export * from "./controller/controller";
+
 export * from "./vehicle/sensor";
 export * from "./vehicle/car";
+
+export * from "./terrain/openCircuit";
+export * from "./terrain/ovalCircuit";
+
+export * from "./scene/basicScene";
