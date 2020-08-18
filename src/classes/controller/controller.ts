500fae640140f3b877b2e07863d861ff51cb1367
bananacocodrilo <antonio.mkp@gmail.com>
2020-08-18 08:42:20 +0200

    ✨ Add keyboard control

--- src/classes/controller/controller.ts
+++ src/classes/controller/controller.ts
@@ -9,18 +9,43 @@ export class Controller {
     throttle: 0,
     steering: 0,
   };
+  subscription: any;
+
+  _receiveKeyboardEvent = (event: KeyboardEvent) => {
+    switch (event.key) {
+      case "a":
+      case "A":
+        this.controlState.steering = event.type === "keydown" ? -100 : 0;
+        break;
+      case "d":
+      case "D":
+        this.controlState.steering = event.type === "keydown" ? 100 : 0;
+        break;
+      case "w":
+      case "W":
+        this.controlState.throttle = event.type === "keydown" ? 100 : 0;
+        break;
+      case "s":
+      case "S":
+        this.controlState.throttle = event.type === "keydown" ? -100 : 0;
+        break;
+      default:
+        return;
+    }
+    this.myObservers.forEach((observer) => observer.onNext(this.controlState));
+  };
 
   constructor(id: string) {
     this.id = `Controller-${id}`;
     this.observable = Observable.create(this.subscribe);
     this.myObservers = [];
 
-    setInterval(() => {
-      this.controlState.throttle = -100 + Math.random() * 200;
-      this.controlState.steering = -100 + Math.random() * 200;
-
-      this.myObservers.forEach((observer) => observer.onNext(this.controlState));
-    }, 1000);
+    this.subscription = Observable.fromEvent(document, "keydown").subscribe(
+      this._receiveKeyboardEvent
+    );
+    this.subscription = Observable.fromEvent(document, "keyup").subscribe(
+      this._receiveKeyboardEvent
+    );
   }
 
   subscribe = (observer: Observer<CarCrontrol>) => {
